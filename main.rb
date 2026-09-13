fd806db4d0eaf97647580d969c391a9aaf951aad
Edwin-Create <edwinwanyoike9999@gmail.com>
2026-09-13 15:45:15 +0300

    Completed a driver script The driver script: Confirms that the tree is balanced by calling #balanced? Prints out all elements in level, pre, post, and in order Unbalances the tree by adding several numbers whose value is more than 100 Confirms that the tree is unbalanced by calling #balanced? Balances the tree by calling #rebalance Confirms that the tree is balanced by calling #balanced?

--- main.rb
+++ main.rb
@@ -1,7 +1,47 @@
 require_relative './lib/node'
 require_relative './lib/tree'
 
-tree = Tree.new([1, 2, 3, 4, 5])
+tree = Tree.new(Array.new(15) { rand(1..100) })
 
-tree.level_order.each { |value| puts value }
 tree.pretty_print
+
+puts 'Initially balanced:'
+puts tree.balanced?
+
+puts 'Level order:'
+tree.level_order { |value| puts value }
+
+puts 'Preorder:'
+tree.preorder { |value| puts value }
+
+puts 'Postorder:'
+tree.postorder { |value| puts value }
+
+puts 'Inorder:'
+tree.inorder { |value| puts value }
+
+5.times do
+  tree.insert(rand(101..200))
+end
+
+puts 'After adding numbers:'
+puts tree.balanced?
+
+tree.rebalance
+
+puts 'After rebalancing:'
+puts tree.balanced?
+
+tree.pretty_print
+
+puts 'Level order:'
+tree.level_order { |value| puts value }
+
+puts 'Preorder:'
+tree.preorder { |value| puts value }
+
+puts 'Postorder:'
+tree.postorder { |value| puts value }
+
+puts 'Inorder:'
+tree.inorder { |value| puts value }
